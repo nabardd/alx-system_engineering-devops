db4b65e64c3c9dc25d32a026c34185f61f42d16f
nabardd <ojebiyidaviddaniel@gmail.com>
2023-04-24 17:19:17 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install a package using puppet-lint
 
-exec { 'puppet-lint':
-    command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0'
+exec { 'pip3':
+    command => 'install flask==2.1.0'
 }
